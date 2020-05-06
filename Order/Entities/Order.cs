a803cbc263941cb4c6e34da4151ed212e8782673
Marlon <marlonsmoreira@gmail.com>
2020-05-06 08:43:08 -0300

    Criação Classe Order

---
+++ Order/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Order.Entities.Enum;
+
+namespace Order.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+    }
+}
